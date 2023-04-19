4c796654be7b51930fda989cc23748c2155961eb
unknown <cmac@arquitecturajava.com>
2023-04-19 11:21:39 +0200

    bucle do while

---
+++ src/App7.java
@@ -0,0 +1,12 @@
+public class App7 {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+
+        do  {
+
+            i++;
+            System.out.println("hola" + i);
+        }while (i<10);
+
+    }
+}
