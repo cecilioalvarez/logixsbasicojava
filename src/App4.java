427e3379fda46476ff40c046dcd897c67d9eb022
unknown <cmac@arquitecturajava.com>
2023-04-19 10:57:03 +0200

    for bucle

---
+++ src/App4.java
@@ -0,0 +1,18 @@
+public class App4 {
+    public static void main(String[] args) throws Exception {
+      
+    
+        int numero=2;
+        if (numero<5) {
+
+            System.out.println("has suspendido");
+        }else  if (numero==5){
+
+            System.out.println("has aprobado");
+        }else {
+            System.out.println("has hecho un buen examen");
+        }
+
+
+    }
+}
---
+++ src/App5.java
@@ -0,0 +1,12 @@
+public class App5 {
+    public static void main(String[] args) throws Exception {
+      
+    
+       for (int i=0;i<10;i++) {
+
+        System.out.println("hola");
+       }
+
+
+    }
+}
