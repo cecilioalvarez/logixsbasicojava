f963ba93c087d3eb98e014056ac018e1187c96f3
unknown <cmac@arquitecturajava.com>
2023-04-19 11:41:09 +0200

    funciones

--- src/App8.java
+++ src/App8.java
@@ -1,13 +1,15 @@
 public class App8 {
     public static void main(String[] args) throws Exception {
 
-        int suma = 0;
+        System.out.println(sumaNumeros(10));
+    }
 
+    public static int sumaNumeros(int numero) {
+        int suma=0;
         for (int i = 0; i < 10; i++) {
             suma = suma + i;
-            System.out.println(suma);
+           // System.out.println(suma);
         }
-
-        System.out.println(suma);
+        return suma;
     }
 }
