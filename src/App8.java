1b705fcd5b169dd5c2129b30c3c020550b8f8826
unknown <cmac@arquitecturajava.com>
2023-04-19 11:37:58 +0200

    suma

---
+++ src/App8.java
@@ -0,0 +1,13 @@
+public class App8 {
+    public static void main(String[] args) throws Exception {
+
+        int suma = 0;
+
+        for (int i = 0; i < 10; i++) {
+            suma = suma + i;
+            System.out.println(suma);
+        }
+
+        System.out.println(suma);
+    }
+}
