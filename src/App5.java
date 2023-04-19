35f1c65ba87a1f8f26f67aad1468d1def01e9afb
unknown <cmac@arquitecturajava.com>
2023-04-19 11:20:09 +0200

    hile

--- src/App5.java
+++ src/App5.java
@@ -1,7 +1,7 @@
 public class App5 {
     public static void main(String[] args) throws Exception {
       
-    
+    // i++ es igual que i=i+1;  
        for (int i=0;i<10;i++) {
 
         System.out.println("hola");
---
+++ src/App6.java
@@ -0,0 +1,12 @@
+public class App6 {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+
+        while (i < 10) {
+
+            i++;
+            System.out.println("hola" + i);
+        }
+
+    }
+}
