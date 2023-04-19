0bee33b2e8d331f74fb755f8a260e3aa0aa395b7
unknown <cmac@arquitecturajava.com>
2023-04-19 10:40:50 +0200

    ejercicio if

---
+++ src/App3.java
@@ -0,0 +1,16 @@
+public class App3 {
+    public static void main(String[] args) throws Exception {
+      
+    
+        int numero=5;
+        if (numero>=5) {
+
+            System.out.println("has aprobado");
+        }else {
+
+            System.out.println("has suspendido");
+        }
+
+
+    }
+}
