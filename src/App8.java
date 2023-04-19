0ed9a090e273b791ce3f4855ed7a466098148a58
unknown <cmac@arquitecturajava.com>
2023-04-19 11:45:03 +0200

    un array sencillo

--- src/App8.java
+++ src/App8.java
@@ -1,12 +1,18 @@
 public class App8 {
     public static void main(String[] args) throws Exception {
 
+        //lo he llamado con el 10
         System.out.println(sumaNumeros(10));
+         //lo he llamado con el 10
+         System.out.println(sumaNumeros(3));
     }
 
+    // bloque de codigo que nosotros podemos reutilizar en
+    // nuestro programa
     public static int sumaNumeros(int numero) {
         int suma=0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numero; i++) {
+            //suma=suma+=i
             suma = suma + i;
            // System.out.println(suma);
         }
---
+++ src/App9.java
@@ -0,0 +1,13 @@
+public class App9 {
+    public static void main(String[] args) throws Exception {
+       
+        int [] lista= new int[] {1,3,5,8};
+
+        for (int i=0;i<lista.length;i++) {
+
+            System.out.println(lista[i]);
+        }
+
+
+    }
+}
