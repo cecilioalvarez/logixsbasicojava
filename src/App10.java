3f0cc50fb982bcd33a10c989eb5088c61bc5ce29
unknown <cmac@arquitecturajava.com>
2023-04-19 12:05:13 +0200

    mayor

---
+++ src/App10.java
@@ -0,0 +1,22 @@
+public class App10 {
+    public static void main(String[] args) throws Exception {
+       
+        // una pequeña mejora
+        int [] lista=  {1,3,5,8};
+
+        int mayor=0;
+
+        for (int i=0;i<lista.length;i++) {
+
+          if (mayor<lista[i]) {
+            // cambiar el valor almacenado en mayor por 
+            // el de la lista ya que la lista es mas grande que el
+            // que tenias almacenado
+            mayor=lista[i];
+          }
+        }
+
+        System.out.println(mayor);
+
+    }
+}
--- src/App9.java
+++ src/App9.java
@@ -1,7 +1,8 @@
 public class App9 {
     public static void main(String[] args) throws Exception {
        
-        int [] lista= new int[] {1,3,5,8};
+        // una pequeña mejora
+        int [] lista=  {1,3,5,8};
 
         for (int i=0;i<lista.length;i++) {
 
