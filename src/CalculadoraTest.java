c3ce53bb43400eb9f82e6fc7696ad368aa6317cc
unknown <cmac@arquitecturajava.com>
2023-04-19 12:34:22 +0200

    pruebas

---
+++ src/CalculadoraTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+    
+
+    @Test
+    public void sumarTest() {
+
+        assertEquals(2, 2, 0);
+
+
+    }
+}
