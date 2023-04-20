f2598a6c7d06aa59dc8b4768495058b863a16c70
unknown <cmac@arquitecturajava.com>
2023-04-20 11:43:42 +0200

    otros

--- src/com/arquitecturajava/manejoclases3/Prueba.java
+++ src/com/arquitecturajava/manejoclases3/Prueba.java
@@ -7,14 +7,26 @@ public class Prueba {
     public static void main(String[] args) {
         
         ArrayList<String> lista= new ArrayList<String>();
+
         lista.add("hola");
         lista.add("que");
         lista.add("tal");
         lista.add("estas");
 
+       
+
         for (String texto: lista) {
 
             System.out.println(texto);
         }
+
+        lista.remove(1);
+        lista.remove(2);
+        System.out.println("********************");
+        for (String texto: lista) {
+
+            System.out.println(texto);
+        }
+        
     }
 }
