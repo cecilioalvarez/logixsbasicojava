0fc78a6394de5ff3e9a2171cb93fe9b8f4d2ae23
unknown <cmac@arquitecturajava.com>
2023-04-20 12:21:55 +0200

    nuevos

--- src/com/arquitecturajava/manejoclases3/AlumnoTest.java
+++ src/com/arquitecturajava/manejoclases3/AlumnoTest.java
@@ -2,6 +2,8 @@ package com.arquitecturajava.manejoclases3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class AlumnoTest {
@@ -25,13 +27,15 @@ public class AlumnoTest {
 
             //arrange
             Alumno alumno= new Alumno("pedro");
-            //act
-            alumno.addNota(new Nota (5));
-            alumno.addNota(new Nota (2));
-            alumno.addNota(new Nota (7));
-            alumno.addNota(new Nota (9));
-            alumno.addNota(new Nota (8));
-          
+            ArrayList<Nota> lista= new ArrayList<Nota>();
+            lista.add(new Nota (5));
+            lista.add(new Nota (2));
+            lista.add(new Nota (7));
+            lista.add(new Nota (9));
+            lista.add(new Nota (8));
+            alumno.setNotas(lista);
+
+            //act 
             Nota notaMayor=alumno.getMejorNota();
 
             //assert 
