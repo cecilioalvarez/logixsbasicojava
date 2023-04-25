208fb513d5558fd814e898bb8cb64321b08cf476
unknown <cmac@arquitecturajava.com>
2023-04-25 11:55:23 +0200

    nuevos jdbc ordenador

--- src/com/arquitecturajava/jdbc/Principal4.java
+++ src/com/arquitecturajava/jdbc/Principal4.java
@@ -5,12 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Principal3 {
+public class Principal4 {
     public static void main(String[] args) {
+        Ordenador o = new Ordenador(1, "modelo1", 500);
+        insertarOrdenador(o);
+
+    }
+
+    public static void insertarOrdenador(Ordenador ordenador) {
+
+        String consultaSQL= "insert into Ordenador (numero,modelo,precio) values ("+ordenador.getNumero()+",'"+ ordenador.getModelo()+"',"+ ordenador.getPrecio()+")";
+        System.out.println(consultaSQL);
+        consultaModificacion(consultaSQL);
+    }
+
+    private static void consultaModificacion(String sql) {
 
         try (Connection conexion = getConexion("mySQL");
                 Statement sentencia = conexion.createStatement();) {
-            sentencia.executeUpdate("insert into Personas (nombre) values ('ana')");
+            sentencia.executeUpdate(sql);
         } catch (SQLException e) {
             System.out.println("ha ocurrido un error");
             System.out.println(e.getMessage());
