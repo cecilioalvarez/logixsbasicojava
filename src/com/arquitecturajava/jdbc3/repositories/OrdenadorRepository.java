2d045005126dedd404f83acb794b907f667e27cf
unknown <cmac@arquitecturajava.com>
2023-04-27 09:31:29 +0200

    limpiar imports

--- src/com/arquitecturajava/jdbc3/repositories/OrdenadorRepository.java
+++ src/com/arquitecturajava/jdbc3/repositories/OrdenadorRepository.java
@@ -1,6 +1,5 @@
 package com.arquitecturajava.jdbc3.repositories;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import com.arquitecturajava.jdbc3.dominio.Ordenador;
@@ -8,19 +7,11 @@ import com.arquitecturajava.jdbc3.dominio.Ordenador;
 public interface OrdenadorRepository {
 
     Ordenador actualizar(Ordenador ordenador); 
-
-    // su propia persistencia
     Ordenador insertar(Ordenador ordenador); 
-
-    // su propia persistencia
     void borrar(Ordenador ordenador);
-
     List<Ordenador> buscarTodos();
-
     List<Ordenador> buscarOrdenadoresBaratos();
-
     Ordenador buscarUno(int numero);
-
     List<Ordenador> buscarPorRangoPrecios(double precioInicial, double precioFinal);
 
 }
